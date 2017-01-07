ad21312374723d808f7c378706b0b8c6765b4c39
Stanislav Sinitskiy <13diver13@gmail.com>
2017-01-07 08:56:16 +0000

    Contact mailer error resolved

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ActionMailer::Base
+    deafalut to: '13diver13@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
         
-        mail (from: email, subject: 'Contact Form Message')
+        mail(from: email, subject: 'Contact Form Message')
     end
 end
